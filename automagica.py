b5f9eb51b98d3377b2faf64c63d9e0b0a87c88fd
Juanjo Conti <jjconti@gmail.com>
2016-11-26 14:42:18 -0300

    Move to main func

--- automagica.py
+++ automagica.py
@@ -26,87 +26,92 @@ DEFAULTS = dict(
 )
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--BASE_FILENAME', default='default')
-parser.add_argument('book_path', help='Carpeta con archivos para un libro.', metavar='carpeta')
-parser.add_argument('--no-split', help='No separar párrafos.', action='store_true')
-parser.add_argument('--pdf', help='Genera la versión pdf del libro.', action='store_true')
-parser.add_argument('--booklet', help='Genera la versión booklet del pdf.', action='store_true')
-parser.add_argument('--epub', help='Genera la versión epub del libro.', action='store_true')
-parser.add_argument('--only-tex', help='Solo genera el archivo latex.', action='store_true')
-parser.add_argument('--sections', help='Usar secciones en lugar de capítulos como elemento principal.', action='store_true')
-parser.add_argument('--new-page-before-sections', help='Forzar página nueva en las secciones principales.', action='store_true')
-parser.add_argument('--TITLE', default='TITLE')
-parser.add_argument('--SUBTITLE', default='')
-parser.add_argument('--AUTHOR', default='AUTHOR')
-parser.add_argument('--FONT_SIZE', default=11)
-parser.add_argument('--PAGE_SIZE', default='a5paper')
-parser.add_argument('--YEAR', default=datetime.now().year)
-parser.add_argument('--URL', default='')
-parser.add_argument('--exclude-index', action='store_true')
-args = parser.parse_args()
-book_path = args.book_path
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--BASE_FILENAME', default='default')
+    parser.add_argument('book_path', help='Carpeta con archivos para un libro.', metavar='carpeta')
+    parser.add_argument('--no-split', help='No separar párrafos.', action='store_true')
+    parser.add_argument('--pdf', help='Genera la versión pdf del libro.', action='store_true')
+    parser.add_argument('--booklet', help='Genera la versión booklet del pdf.', action='store_true')
+    parser.add_argument('--epub', help='Genera la versión epub del libro.', action='store_true')
+    parser.add_argument('--only-tex', help='Solo genera el archivo latex.', action='store_true')
+    parser.add_argument('--sections', help='Usar secciones en lugar de capítulos como elemento principal.', action='store_true')
+    parser.add_argument('--new-page-before-sections', help='Forzar página nueva en las secciones principales.', action='store_true')
+    parser.add_argument('--TITLE', default='TITLE')
+    parser.add_argument('--SUBTITLE', default='')
+    parser.add_argument('--AUTHOR', default='AUTHOR')
+    parser.add_argument('--FONT_SIZE', default=11)
+    parser.add_argument('--PAGE_SIZE', default='a5paper')
+    parser.add_argument('--YEAR', default=datetime.now().year)
+    parser.add_argument('--URL', default='')
+    parser.add_argument('--exclude-index', action='store_true')
+    args = parser.parse_args()
+    book_path = args.book_path
 
 
-class EmptyConfig(object):
-    pass
+    class EmptyConfig(object):
+        pass
 
-if not os.path.isdir(book_path):
-    print('El argumento debe ser un directorio')
-    exit()
-config_file = os.path.join(book_path, 'config.py')
-if os.path.isfile(config_file):
-    config = imp.load_source('config', config_file)
-else:
-    config = EmptyConfig()
-    config.CONFIGS = {}
+    if not os.path.isdir(book_path):
+        print('El argumento debe ser un directorio')
+        exit()
+    config_file = os.path.join(book_path, 'config.py')
+    if os.path.isfile(config_file):
+        config = imp.load_source('config', config_file)
+    else:
+        config = EmptyConfig()
+        config.CONFIGS = {}
 
-VARS = DEFAULTS.copy()
-VARS.update(config.CONFIGS)
-for k,v in args._get_kwargs():
-    if not VARS.get(k):
-        VARS[k] = v
+    VARS = DEFAULTS.copy()
+    VARS.update(config.CONFIGS)
+    for k,v in args._get_kwargs():
+        if not VARS.get(k):
+            VARS[k] = v
 
 
-index_path = os.path.join(book_path, 'index.txt')
+    index_path = os.path.join(book_path, 'index.txt')
 
-split_paragraphs = not VARS['no_split']
-if os.path.isfile(index_path):
-    with open(index_path, 'r') as f:
-        content = ''
-        for filename in f.readlines():
-            if VARS['no_split']:
-                content += latex_chapter(os.path.join(book_path, filename).strip(), split_paragraphs)
-            else:
-                content += latex_single(os.path.join(book_path, filename).strip(), split_paragraphs, VARS['sections'], VARS['new_page_before_sections'])
-        VARS['CONTENT'] = content
-else:
-    text_files = [f for f in glob.glob(os.path.join(book_path, '*.txt')) if not f.endswith('words.txt')]
-    if text_files:
-        VARS['CONTENT'] = latex_single(text_files[0], split_paragraphs, VARS['sections'], VARS['new_page_before_sections'])
+    split_paragraphs = not VARS['no_split']
+    if os.path.isfile(index_path):
+        with open(index_path, 'r') as f:
+            content = ''
+            for filename in f.readlines():
+                if VARS['no_split']:
+                    content += latex_chapter(os.path.join(book_path, filename).strip(), split_paragraphs)
+                else:
+                    content += latex_single(os.path.join(book_path, filename).strip(), split_paragraphs, VARS['sections'], VARS['new_page_before_sections'])
+            VARS['CONTENT'] = content
+    else:
+        text_files = [f for f in glob.glob(os.path.join(book_path, '*.txt')) if not f.endswith('words.txt')]
+        if text_files:
+            VARS['CONTENT'] = latex_single(text_files[0], split_paragraphs, VARS['sections'], VARS['new_page_before_sections'])
 
-sep_path = os.path.join(book_path, 'words.txt')
-if os.path.isfile(sep_path):
-    with open(sep_path, 'r') as f:
-        hyphenation = ''
-        for word in f.readlines():
-            hyphenation += latex_hyphenation(word.strip())
-        VARS['HYPHENATION'] = hyphenation
+    sep_path = os.path.join(book_path, 'words.txt')
+    if os.path.isfile(sep_path):
+        with open(sep_path, 'r') as f:
+            hyphenation = ''
+            for word in f.readlines():
+                hyphenation += latex_hyphenation(word.strip())
+            VARS['HYPHENATION'] = hyphenation
 
-TEMPLATE = 'template.tex'
+    TEMPLATE = 'template.tex'
 
-template = latex_env.get_template(TEMPLATE)
+    template = latex_env.get_template(TEMPLATE)
 
-base_filename = VARS['BASE_FILENAME']
-tex_file = filepath(book_path, base_filename, 'tex')
+    base_filename = VARS['BASE_FILENAME']
+    tex_file = filepath(book_path, base_filename, 'tex')
 
-with open(tex_file, 'w') as f:
-    f.write(template.render(**VARS))
+    with open(tex_file, 'w') as f:
+        f.write(template.render(**VARS))
 
-if not args.only_tex:
-    if args.pdf or not args.epub:
-        pdf_file = generate_pdf(book_path, base_filename, tex_file)
-        if args.booklet:
-            generate_booklet(pdf_file, filepath(book_path, base_filename, 'booklet.pdf'))
-    if args.epub:
-        generate_epub(book_path, base_filename, tex_file)
+    if not args.only_tex:
+        if args.pdf or not args.epub:
+            pdf_file = generate_pdf(book_path, base_filename, tex_file)
+            if args.booklet:
+                generate_booklet(pdf_file, filepath(book_path, base_filename, 'booklet.pdf'))
+        if args.epub:
+            generate_epub(book_path, base_filename, tex_file)
+
+
+if __name__ == '__main__':
+    main()
