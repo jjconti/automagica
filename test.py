c068e2de1c17f88ed7a1db3f873395d63bae6b5a
Francisco Rivera <franmrivera@gmail.com>
2016-11-28 23:07:30 -0300

    work

--- test.py
+++ test.py
@@ -1,62 +1,56 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import glob
 import shutil
 import os
 import subprocess
+from unittest import TestCase
 
 
-TESTS_DIR = 'tests_data'
-EXAMPLES = {'ejemplo': [[]],
-            'ejemplo_no_config': [[]],
-            'ejemplo_2': [[]],
-            'ejemplo_single': [[], ['--exclude-index', '--BASE_FILENAME=index_excluded']]}
+# legacy tests
+# don't blame me :P
 
 
-def run_and_assert(cmd):
-    print("Running: {}".format(cmd))
-    proc = subprocess.Popen(cmd)
-    proc.communicate()
-    retcode = proc.returncode
-    assert retcode == 0
+class BuildTest(TestCase):
 
+    TESTS_DIR = 'tests_data'
+    EXAMPLES = {
+        'ejemplo': [[]],
+        'ejemplo_no_config': [[]],
+        'ejemplo_2': [[]],
+        'ejemplo_single': [[], ['--exclude-index', '--BASE_FILENAME=index_excluded']]
+    }
 
-def clean():
-    for ex in EXAMPLES.keys():
-        dir = os.path.join(TESTS_DIR, ex)
-        if os.path.isdir(dir):
-            shutil.rmtree(dir)
+    @classmethod
+    def tearDownClass(cls):
+        for ex in cls.EXAMPLES.keys():
+            dir = os.path.join(cls.TESTS_DIR, ex)
+            if os.path.isdir(dir):
+                shutil.rmtree(dir)
 
+    @staticmethod
+    def run_and_assert(cmd):
+        print("Running: {}".format(cmd))
+        proc = subprocess.Popen(cmd)
+        proc.communicate()
+        return proc.returncode
 
-def run_tests():
-    for ex in EXAMPLES.keys():
-        new_dir = os.path.join(TESTS_DIR, ex)
-        os.mkdir(new_dir)
-        for f in glob.glob(os.path.join(ex, '*.txt')) + [os.path.join(ex, 'config.py')]:
-            if os.path.isfile(f):
-                shutil.copy(f, new_dir)
-        for params in EXAMPLES[ex]:
-            cmd = ['python', 'automagica.py', '--only-tex'] + params + [os.path.join(TESTS_DIR, ex)]
-            run_and_assert(cmd)
+    def test_build(self):
+        for ex in self.EXAMPLES.keys():
+            new_dir = os.path.join(self.TESTS_DIR, ex)
+            os.mkdir(new_dir)
+            for f in glob.glob(os.path.join(ex, '*.txt')) + [os.path.join(ex, 'config.py')]:
+                if os.path.isfile(f):
+                    shutil.copy(f, new_dir)
+            for params in self.EXAMPLES[ex]:
+                cmd = ['python', 'automagica.py', '--only-tex'] + params + [os.path.join(self.TESTS_DIR, ex)]
+                self.run_and_assert(cmd)
 
-    cmd = ['diff', 'tests_data/ejemplo/jungla.tex', 'tests_data/jungla.tex']
-    run_and_assert(cmd)
-    cmd = ['diff', 'tests_data/ejemplo_2/sueltos.tex', 'tests_data/sueltos.tex']
-    run_and_assert(cmd)
-    cmd = ['diff', 'tests_data/ejemplo_single/default.tex', 'tests_data/default.tex']
-    run_and_assert(cmd)
-    cmd = ['diff', 'tests_data/ejemplo_single/index_excluded.tex', 'tests_data/index_excluded.tex']
-    run_and_assert(cmd)
-
-
-if __name__ == '__main__':
-    try:
-        run_tests()
-    except Exception as e:
-        print e
-        exit(1)
-    else:
-        exit(0)
-    finally:
-        clean()
+        cmd = ['diff', 'tests_data/ejemplo/jungla.tex', 'tests_data/jungla.tex']
+        self.assertEqual(self.run_and_assert(cmd), 0)
+        cmd = ['diff', 'tests_data/ejemplo_2/sueltos.tex', 'tests_data/sueltos.tex']
+        self.assertEqual(self.run_and_assert(cmd), 0)
+        cmd = ['diff', 'tests_data/ejemplo_single/default.tex', 'tests_data/default.tex']
+        self.assertEqual(self.run_and_assert(cmd), 0)
+        cmd = ['diff', 'tests_data/ejemplo_single/index_excluded.tex', 'tests_data/index_excluded.tex']
+        self.assertEqual(self.run_and_assert(cmd), 0)
