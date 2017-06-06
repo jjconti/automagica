3ed44f4a4ab9a7ef652e66231c4b1ed1ab708dc8
Juanjo Conti <jjconti@gmail.com>
2017-06-06 11:47:08 -0300

    Read template.tex from book_path if any

--- automagica.py
+++ automagica.py
@@ -95,8 +95,11 @@ def main():
             VARS['HYPHENATION'] = hyphenation
 
     TEMPLATE = 'template.tex'
-
-    template = latex_env.get_template(TEMPLATE)
+    local_template_path = os.path.join(book_path, 'template.tex')
+    if os.path.isfile(local_template_path):
+        template = latex_env.from_string(open(local_template_path).read())
+    else:
+        template = latex_env.get_template(TEMPLATE)
 
     base_filename = VARS['BASE_FILENAME']
     tex_file = filepath(book_path, base_filename, 'tex')
